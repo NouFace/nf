be9b24ffb6f85d20e391c7b95ef01531cdd3d3e4
yesntface <42227386+yesntface@users.noreply.github.com>
2018-12-17 00:26:20 +0400

    Update mot.js

--- docs/mot.js
+++ docs/mot.js
@@ -2,11 +2,11 @@ let userScore = 0;
 let botScore = 0;
 const _userScore = document.getElementById('user-score')
 const _botScore = document.getElementById('bot-score');
-const _scoreboard= document.querySelector('playerCol');
 const _result = document.querySelector('.result')
 const _rock = document.getElementById('rock');
 const _paper = document.getElementById('paper');
-const _scissors = document.getElementById('scissors')
+const _scissors = document.getElementById('scissors');
+const _miho = document.getElementById('res');
 
 function botChoices(){
     const choices =['rock','paper', 'scissors'];
@@ -36,14 +36,24 @@ function win (user, bot){
     userScore++;
     _userScore.innerHTML = userScore;
     _result.innerHTML =   user + '   beats   ' + bot + ', You  Won !!! ';
+    document.getElementById(user).classList.add('blueW');
+    setTimeout(function() {
+        document.getElementById(user).classList.remove('blueW')},100);
 }
 function draw (user, bot){
     _result.innerHTML =   "It's   a   draw !";
+    document.getElementById(user).classList.add('yellowD');
+    setTimeout(function() {
+    document.getElementById(user).classList.remove('yellowD')},100);
+
 }
 function lose (user, bot){
     botScore++;
     _botScore.innerHTML = botScore;
     _result.innerHTML =   bot + ' beats ' + user + ',  You  lost ! ';
+    document.getElementById(user).classList.add('redL')
+    setTimeout(function() {
+        document.getElementById(user).classList.remove('redL')},100);
 }
 
 
@@ -64,4 +74,6 @@ function main() {
         game("scissors");
 
     })
+    
+
 }
